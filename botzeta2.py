4c819e38dc62bb553043ee1f014d5d41cd7cf52e
zaid <165736988+copitotactico@users.noreply.github.com>
2024-04-29 16:46:47 -0600

    Create botzeta2.py

---
+++ botzeta2.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined in {member.joined_at}')
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.group()
+
+
+
+
+bot.run("MTIyOTU5ODEyOTMzOTUwMjY4Mw.GWV5yt.nfP1wjdIUEf1FA1Y_nrtFz3e9uH-28oDtYFImA")
